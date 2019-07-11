1337118e18bf8c774132622c6905d782155974b8
eropsyren <eropsyren@gmail.com>
2019-07-11 18:29:40 +0200

    add Player struct to play a sound

---
+++ src/player.rs
@@ -0,0 +1,23 @@
+extern crate rodio;
+
+use rodio::Sink;
+
+pub struct Player {
+    sink: Sink,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        let device = rodio::default_output_device().expect("Error: unable to obtain default outut device");
+        let sink = Sink::new(&device);
+
+        // Add a dummy source of the sake of the example.
+        let source = rodio::source::SineWave::new(440);
+        
+        sink.append(source);
+
+        Player {
+            sink,
+        }
+    }
+}
