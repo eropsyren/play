557183625374ef96ec1da7b7eaeb1af73e17571d
eropsyren <eropsyren@gmail.com>
2019-07-15 21:28:10 +0200

    rework state struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod audio;
 mod input;
 mod player;
-mod screen;
 mod state;
 
 pub use audio::Audio;
 pub use input::InputHandler;
 pub use player::Player;
-pub use screen::Screen;
 pub use state::State;
--- src/state.rs
+++ src/state.rs
@@ -3,101 +3,38 @@ use crate::Audio;
 pub struct State {
     audios: Vec<Audio>,
     pointed: usize,
-    loaded: usize,
-    min: usize,
-    max: usize,
-    clean: bool,
-    paused: bool,
 }
 
 impl State {
-    pub fn new(audios: Vec<Audio>, pointed: usize, loaded: usize, audio_span: usize, paused: bool) -> Self {
-        let max = if audios.len() > audio_span {
-            audio_span
+    pub fn new(audios: Vec<Audio>) -> Option<Self> {
+        if audios.len() == 0 {
+            None
         } else {
-            audios.len()
-        };
-
-        State {
-            audios,
-            pointed,
-            loaded,
-            min: 0,
-            max,
-            clean: false,
-            paused,
+            Some(State { audios, pointed: 0 })
         }
     }
 
-    pub fn set_paused(&mut self, value: bool) {
-        self.clean = false;
-        self.paused = value;
+    pub fn is_pointing_to_last(&self) -> bool {
+        self.audios.len() - 1 == self.pointed
     }
 
-    pub fn is_paused(&self) -> bool {
-        self.paused
-    }
-
-    pub fn set_loaded_to_next(&mut self) {
-        self.clean = false;
-
-        if self.loaded == self.audios.len() - 1 {
-            self.loaded = 0;
-        } else {
-            self.loaded += 1;
-        }
-    }
-
-    pub fn is_clean(&self) -> bool {
-        self.clean
-    }
-
-    pub fn clean(&mut self) {
-        self.clean = true;
-    }
-
-    pub fn audios(&self) -> &[Audio] {
-        &self.audios[self.min..self.max]
-    }
-
-    pub fn loaded(&self) -> &Audio {
-        &self.audios[self.loaded]
-    }
-
-    pub fn set_loaded_to_pointed(&mut self) {
-        self.clean = false;
-        self.loaded = self.pointed;
-    }
-
-    pub fn pointed(&self) -> usize {
-        self.pointed - self.min
-    }
-
-    pub fn prev(&mut self) {
-        self.clean = false;
-
-        if self.pointed == self.min {
-            if self.min > 0 {
-                self.min -= 1;
-                self.max -= 1;
-                self.pointed -= 1;
-            }
-        } else {
-            self.pointed -= 1;
-        }
-    }
-
-    pub fn next(&mut self) {
-        self.clean = false;
-
-        if self.pointed == self.max - 1 {
-            if self.max < self.audios.len() {
-                self.min += 1;
-                self.max += 1;
-                self.pointed += 1;
-            }
-        } else {
+    pub fn point_to_next(&mut self) -> &Self {
+        if self.pointed < self.audios.len() {
             self.pointed += 1;
+        } else {
+            self.pointed = 0;
         }
+
+        self
+    }
+
+    pub fn point_to_first(&mut self) -> &Self {
+        self.pointed = 0;
+
+        self
+    }
+
+    pub fn pointed(&self) -> &Audio {
+        &self.audios[self.pointed]
     }
 }
